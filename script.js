799370edea991f472262ab37500b49ba18ca3724
SOMEONEYOUKNOWDOTCOM <officialduties14@gmail.com>
2025-11-04 16:44:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function handleLogin() {
   const usernameInput = document.getElementById('jafar').value;
   const username = usernameInput.toLowerCase();
   const password = document.getElementById('oli').value;
-
+  const message = document.getElementById('bro').value;
   // Example: Log values or validate
   if (username === "admin" && password === "1234") {
     alert("Login successful!");
+    message.style.display = "block";
   } else {
     alert("Invalid username or password.");
   }
