97565636356c180263d71bfff4baa04d80410999
SOMEONEYOUKNOWDOTCOM <officialduties14@gmail.com>
2025-11-04 16:46:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function handleLogin() {
   const usernameInput = document.getElementById('jafar').value;
   const username = usernameInput.toLowerCase();
   const password = document.getElementById('oli').value;
-  const message = document.getElementById('bro').value;
+  const message = document.getElementById('bro');
   // Example: Log values or validate
   if (username === "admin" && password === "1234") {
     alert("Login successful!");
